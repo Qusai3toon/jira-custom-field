ff6b6e2f8d36634cbf209eda0c2059df7b0cc250
Baha'a Aldeen Alnatour <bahaddin@inspirejo.com>
2023-05-08 20:32:49 +0300

    The custom field is now done.
    Only user that are members of the '' group are gonna see the value of the custom field.

--- src/main/java/com/example/plugins/tutorial/jira/customfields/MoneyCustomField.java
+++ src/main/java/com/example/plugins/tutorial/jira/customfields/MoneyCustomField.java
@@ -1,42 +1,51 @@
 package com.example.plugins.tutorial.jira.customfields;
 
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.customfields.impl.AbstractSingleFieldType;
-import com.atlassian.jira.issue.customfields.persistence.PersistenceFieldType;
-import com.atlassian.jira.security.JiraAuthenticationContext;
-import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
-import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
-import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
+import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
 import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
 import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
-import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
-import com.atlassian.jira.user.*;
-import com.atlassian.jira.component.ComponentAccessor;
-
-
+import com.atlassian.jira.issue.customfields.persistence.PersistenceFieldType;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
+import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.jira.security.groups.GroupManager;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.user.util.UserManager;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
 
 import javax.annotation.Nonnull;
 import java.math.BigDecimal;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-//import com.atlassian.sal.api.user.*;
 
 @Scanned
 public class MoneyCustomField extends AbstractSingleFieldType<BigDecimal> {
 
     JiraAuthenticationContext jiraAuthenticationContext;
-    public boolean viewField = false;
+
+    public boolean viewField;
+    final String jIRA_ADMINISTRATORS_GROUP = "jira-administrators";
 
     public MoneyCustomField(
             @JiraImport CustomFieldValuePersister customFieldValuePersister,
             @JiraImport GenericConfigManager genericConfigManager,
             @ComponentImport JiraAuthenticationContext jiraAuthenticationContext) {
-        super(customFieldValuePersister, genericConfigManager);
-        this.jiraAuthenticationContext = jiraAuthenticationContext;
 
+        super(customFieldValuePersister, genericConfigManager);
+
+        this.jiraAuthenticationContext = jiraAuthenticationContext;
+        UserManager userManager = ComponentAccessor.getUserManager();
+        ApplicationUser user = userManager.getUserByName(jiraAuthenticationContext.getLoggedInUser().getName());
+        GroupManager groupManager = ComponentAccessor.getGroupManager();
+        Set<String> currentUserGroups = (Set<String>) groupManager.getGroupNamesForUser(Objects.requireNonNull(user));
+        this.viewField = currentUserGroups.contains(jIRA_ADMINISTRATORS_GROUP);
     }
+
 
     @Override
     public String getStringFromSingularObject(final BigDecimal singularObject) {
@@ -71,27 +80,6 @@ public class MoneyCustomField extends AbstractSingleFieldType<BigDecimal> {
     @Override
     protected BigDecimal getObjectFromDbValue(@Nonnull final Object databaseValue)
             throws FieldValidationException {
-        System.err.println("getObjectFromDbValue(): " + (String) databaseValue);
-        UserDetails userDetails = new UserDetails("admin", "admin");
-        System.out.println("DirectoryID: " + userDetails.getDirectoryId().toString());
-        System.out.println("username: " + userDetails.getUsername());
-        System.out.println("Password: " + userDetails.getPassword());
-        System.out.println("Display name: " + userDetails.getDisplayName());
-//        System.out.println(jiraAuthenticationContext.getLoggedInUser());
-        System.out.println("ID: "
-                + jiraAuthenticationContext.getLoggedInUser().getId().toString()
-                + " Name " + jiraAuthenticationContext.getLoggedInUser().getName());
-
-//        Collection<String> user_groups new Collection<String>;
-
-        String current_name = jiraAuthenticationContext.getLoggedInUser().getName();
-
-        Set<String> groups = getUserGroups(current_name);
-
-        for (String group : groups) {
-            System.out.println("Current user groups are: " + group);
-        }
-
         return getSingularObjectFromString((String) databaseValue);
     }
 
@@ -106,5 +94,22 @@ public class MoneyCustomField extends AbstractSingleFieldType<BigDecimal> {
         ApplicationUser user = userManager.getUserByName(name);
         GroupManager groupManager = ComponentAccessor.getGroupManager();
         return (Set<String>) groupManager.getGroupNamesForUser(Objects.requireNonNull(user));
+    }
+
+    private boolean isLoggedInUserAdminUserAdmin() {
+        String current_name = null;
+        if (jiraAuthenticationContext != null) {
+            current_name = jiraAuthenticationContext.getLoggedInUser().getName();
+        }
+        Set<String> currentUserGroups = getUserGroups(current_name);
+        return currentUserGroups.contains("jira-administrators");
+    }
+
+    @Nonnull
+    @Override
+    public Map<String, Object> getVelocityParameters(Issue issue, CustomField field, FieldLayoutItem fieldLayoutItem) {
+        Map<String, Object> params = super.getVelocityParameters(issue, field, fieldLayoutItem);
+        params.put("viewField", viewField);
+        return params;
     }
 }
