824d416887a9e698dc8d6a6dfe8588c2fab9fbb0
Qusai Atoon <qusai.atoon@gmail.com>
2023-05-12 19:21:07 +0300

    New Custom Field.
    Added The Custom Field Called Story Cost, which is gonna get the sum of all money values of all tasks related to this story. and if there aren't, then it's going to be 0.00

---
+++ src/test/java/ut/com/example/plugins/tutorial/jira/jira/customfields/StoryCostTest.java
@@ -0,0 +1,34 @@
+package ut.com.example.plugins.tutorial.jira.jira.customfields;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.example.plugins.tutorial.jira.jira.customfields.StoryCost;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class StoryCostTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //StoryCost testClass = new StoryCost();
+
+        throw new Exception("StoryCost has no tests!");
+
+    }
+
+}
