808c4015f1abc0e98bc37a7c05655a821e42ae73
Qusai3toon <71739845+Qusai3toon@users.noreply.github.com>
2023-06-16 19:29:43 +0300

    Update EvaluationServlet.java

--- src/main/java/com/example/plugins/tutorial/jira/servlets/EvaluationServlet.java
+++ src/main/java/com/example/plugins/tutorial/jira/servlets/EvaluationServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
+import java.util.Date; 
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
